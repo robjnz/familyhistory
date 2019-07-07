c065a8f048b015bd1b6d20a4f8f81f0872c13d2b
robin harris <robjnz@outlook.com>
2019-07-07 19:20:46 +0100

    add google

--- components/TheFooter/TheFooter.vue
+++ components/TheFooter/TheFooter.vue
@@ -1,5 +1,16 @@
 <template>
 <section>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-84231316-5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-84231316-5');
+</script>
+
+
 
 
 </section>

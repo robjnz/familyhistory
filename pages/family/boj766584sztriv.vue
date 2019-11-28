0028900cdcfc11be94b30cba9ee56e2bf7d5e5c8
robin harris <robjnz@outlook.com>
2019-11-28 06:31:32 +0000

    add bravehost

--- pages/family/boj766584sztriv.vue
+++ pages/family/boj766584sztriv.vue
@@ -80,6 +80,8 @@ From Kutno, Plock & Warsaw Poland.
 </b-card>
 
 </b-card-group>
+<button style="border-radius:5px;padding:5px 10px;background:#000;border:0;color:#fff;" onclick="top.location.href = 'https://pub30.bravenet.com/passwd/logout.php?usernum=2539049398';" target="_top">Logout</button>
+
       </div>
   </b-container>
 </section>
--- pages/login.vue
+++ pages/login.vue
@@ -5,15 +5,23 @@
       <div>
         <b-card bg-variant="danger" text-variant="white" header="Private family History Pages" class="text-center">
 
-          <!--
-          ><b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
--->
-<password />
+
 
 <input type="button" value="Enter Protected Page" onClick="passWord()">
-
+<!-- Start Bravenet.com Service Code -->
+<div style="text-align: right; width:210px; padding:5px;font: normal 13px arial;">
+  <form action="https://pub30.bravenet.com/passwd/show.php" method="post" target="_top">
+    <input type="hidden" name="usernum" value="2539049398">
+    <h3 style="text-align:center;margin:5px;">Login for Access</h3>
+    <div style="padding:5px;">Username: <input type="text" name="uname" size="15" maxlength="30" style="width: 100px;padding:5px;border-radius:5px;border:1px solid #ccc;"></div>
+    <div style="padding:5px;">Password: <input type="password" name="passwd" size="15" maxlength="30" style="width: 100px;padding:5px;border-radius:5px;border:1px solid #ccc;"></div>
+    <div style="padding:5px;"><a style="color:#00b" href="https://pub30.bravenet.com/passwd/requestpwd.php?usernum=2539049398" target="_top">Request an Account</a> &nbsp; <input type="submit" name="submit" value="Login" style="border-radius:5px;padding:5px 10px;background:#000;border:0;color:#fff;"></div>
+    <div style="padding:5px; font-size:11px;"><a href="https://www.bravenet.com" style="color:#00b;">Powered by Bravenet</a></div>
+  </form>
+</div>
+<!-- End Bravenet.com Service Code -->
 </b-card>
-
+<button style="border-radius:5px;padding:5px 10px;background:#000;border:0;color:#fff;" onclick="top.location.href = 'https://pub30.bravenet.com/passwd/logout.php?usernum=2539049398';" target="_top">Logout</button>
       </div>
   </b-container>
 </section>
@@ -22,13 +30,12 @@
 
 <script>
 
-import password from '@/pages/family/password'
 
 
 export default {
 
   components: {
-password,
+
 }
 }
 </script>
